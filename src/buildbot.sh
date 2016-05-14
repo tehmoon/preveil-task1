1b23bb55e82770f98592b6511c36e23d2b27396c
Edouard Buschini <edouard.buschini@gmail.com>
2016-05-14 19:09:47 +0200

    Continuing work on the buildbot

--- src/buildbot.sh
+++ src/buildbot.sh
@@ -2,9 +2,12 @@
 set -e
 
 GIT_URL=${1}
+GIT_BRANCH=dev
+TMP_DIR=tmp/
 
 usage() {
 	MSG=${1}
+
 	[ ! "x${MSG}" = "x" ] && echo ${MSG} > /dev/stderr
 
 	echo "./bin/buildbot.sh <git_url>" > /dev/stderr
@@ -14,9 +17,16 @@ usage() {
 
 [ "x${GIT_URL}" = "x" ] && usage
 
-echo $GIT_URL
+clean() {
+	echo -n "Cleaning tmp dir... "
+
+	[ "x${TMP_DIR}" = "x" ] && echo -e "NOK" > /dev/stderr && exit 2
+	rm -rf ${TMP_DIR}/*
+	echo "OK."
+}
 
 start() {
+	echo ${GIT_TMP}
 	while true
 	do
 		echo "sleep"
@@ -24,4 +34,27 @@ start() {
 	done
 }
 
-start
+checkout() {
+	git clone ${GIT_URL} ${GIT_TMP}
+
+	cd ${GIT_TMP}
+
+	git checkout -b dev
+}
+
+checkout_and_start() {
+	GIT_TMP=$(mktemp -d -p ${TMP_DIR})
+
+	checkout
+	start
+}
+
+check_path() {
+	which git
+}
+
+env
+
+check_path
+clean
+checkout_and_start
