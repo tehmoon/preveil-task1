8105b5e143f50f4c98ddf3b1349e2b4dc10a9ef4
Edouard Buschini <edouard.buschini@gmail.com>
2016-05-14 18:15:25 +0200

    Added usage and start function

--- src/buildbot.sh
+++ src/buildbot.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
-
 set -e
 
+GIT_URL=${1}
 
+usage() {
+	MSG=${1}
+	[ ! "x${MSG}" = "x" ] && echo ${MSG} > /dev/stderr
 
-while true
-do
-  sleep  1
-done
+	echo "./bin/buildbot.sh <git_url>" > /dev/stderr
+
+	exit 2
+}
+
+[ "x${GIT_URL}" = "x" ] && usage
+
+echo $GIT_URL
+
+start() {
+	while true
+	do
+		echo "sleep"
+	  sleep  1
+	done
+}
+
+start
