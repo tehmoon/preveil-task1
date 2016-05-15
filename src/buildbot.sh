3508729e26241a33a8f64f7ffc7eb74d02ee9c89
Edouard Buschini <edouard.buschini@gmail.com>
2016-05-15 21:34:48 +0200

    Added get_git_path in order to fetch task dir

--- src/buildbot.sh
+++ src/buildbot.sh
@@ -3,7 +3,8 @@ set -e
 
 GIT_URL=${1}
 GIT_BRANCH=dev
-TMP_DIR=tmp/
+TMP_DIR=tmp
+TARGET_PATH=targets
 
 usage() {
 	MSG=${1}
@@ -26,12 +27,36 @@ clean() {
 }
 
 start() {
-	echo ${GIT_TMP}
+	CURRENT_COMMIT=$(get_cur_commit)
+
 	while true
 	do
-		echo "sleep"
+		echo ${CURRENT_COMMIT}
+		echo ${TASK_PATH}
+
+		git_refresh && CURRENT_COMMIT=$(get_cur_commit)
+
 	  sleep  1
 	done
+}
+
+git_refresh() {
+	cd ${GIT_TMP}
+
+	git pull origin dev
+	RET=$?
+
+	cd -
+
+  return ${RET}
+}
+
+get_cur_commit() {
+	cd ${GIT_TMP}
+
+	git log --pretty=format:%H -1
+
+	cd -
 }
 
 checkout() {
@@ -39,21 +64,36 @@ checkout() {
 
 	cd ${GIT_TMP}
 
-	git checkout -b dev origin/dev
+	git checkout -b ${GIT_BRANCH} origin/${GIT_BRANCH}
+
+	cd -
+}
+
+get_git_path() {
+	cd ${GIT_TMP}
+
+	REMOTE=$(git remote get-url --push origin)
+
+	echo ${REMOTE} | grep '://' 2>&1 > /dev/null && echo ${REMOTE} | cut -d '/' -f 3- || ( \ # handling pattern like scheme://uri
+	echo ${REMOTE} | grep '@' 2>&1 > /dev/null && echo ${REMOTE} | cut -d '@' -f 2- | sed 's/:/\//' | sed -e 's/\.git$//' ) # handling pattern like username@host:git_url
+
+	cd -
 }
 
 checkout_and_start() {
 	GIT_TMP=$(mktemp -d -p ${TMP_DIR})
 
 	checkout
+
+	GIT_PATH=$(get_git_path)
+	TASK_PATH=${TARGET_DIR}/${GIT_PATH}
+
 	start
 }
 
 check_path() {
 	which git
 }
-
-env
 
 check_path
 clean
